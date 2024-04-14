eb0594fe7e1d302ed59795f6443a6073c25044b1
Carlos Ramos <devcarlosramos@gmail.com>
2024-04-14 17:58:49 -0500

    implement gcp

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -1,21 +1,21 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
-provider "registry.terraform.io/hashicorp/azurerm" {
-  version = "3.98.0"
+provider "registry.terraform.io/hashicorp/google" {
+  version = "5.24.0"
   hashes = [
-    "h1:3r3XaFLvWFiR16p1Qkfi67nygfUROO+vnV2X/UUs3mA=",
-    "zh:012c79f671cff194d769ee53d50c56e7d353d4b78de8fceec9915bcf5955878b",
-    "zh:022d07683f84f8534fa40567860f76da6d15713d678cbc979c1d8cd502bb3246",
-    "zh:12cefd7df62c23c434d853e1824d421b4f18ff683540861376cf37138d70795b",
-    "zh:42626315686f504c59bec02745d7beff2bdfa732c3ecfaca7794f6641fbebfdc",
-    "zh:44708deec3fe13ea7cdd899ee766971fd024c06e2d8e189c30b59bd56ef3a5d3",
-    "zh:54a54fb4c8fc6537aae658503182e03af545e9151afe68da0f254d9b31037c63",
-    "zh:8265688742f6b532f06a3cd0bb14a891eb2277a834cf126f4b483b6de0a8d8fa",
-    "zh:97294d6e502e4e07e0cd8d2669a05d7e5d1f42da542ced299b5d4a3849e8736e",
-    "zh:bf4b48606c53db399f53b3880d94f70fc1701c72fde269472d8231f6dda5cf49",
-    "zh:c5a957d61e4c705e2c94d53c6fff579d5262137b2192562d0dfbbda374891717",
-    "zh:e2a8232f20f74602632b24d50c5c9d21741b0345560e4133bfb38e257d20fb77",
+    "h1:RAuSwFWUzTeGWmPlFA1S6pIULW/zdd5Cjl7Yf2U03zg=",
+    "zh:108d318bd6b5aa6cbfd4f5ace415c92129e37a78dd8ec2a1fe863a07690ac6ee",
+    "zh:12f2dad5c506934c07b655ae6de083374231acfff9b648de4fb8de2564d1c83c",
+    "zh:24bc487ea381a34c7416726f96307c9c30332b49a9937ea55ffb3092783f03b2",
+    "zh:2c0ead7c00f428da762ab0f848d19d6dc4cf1d1da8b8ed9eca560cb0abc0ff6b",
+    "zh:34b0a6f3ec8d1b1d6ae22f1ed00209a96c757f7f63fe9aebaab7a185bf79b66d",
+    "zh:4470e00308b80e071023771d1dc5b02ae249874e1a0fa29e2e701118126791c2",
+    "zh:581ebed948ef9b85d2683be8682cd2f2a2a3c8d0a260c2412197b6d1e9e3f2a7",
+    "zh:974c091fb381dc5a39afb447666221177cae75ba89c2884f02f12a1586a8b5b9",
+    "zh:a9d7a728c94144410f54360c3f8d92e8913b2564779f238cd420196ebf4014cc",
+    "zh:ba20867988cd386709639a2669fec7f3e086d075dc189555b389b7024efb779a",
+    "zh:e79d978f6ece3bb9d35bcabdc163803ff6c9fdffff8d733d431c9edbfe90e016",
     "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
   ]
 }
--- main.tf
+++ main.tf
@@ -1,86 +1,39 @@
-provider "azurerm" {
-  subscription_id = ""
-  client_id       = ""
-  client_secret   = ""
-  tenant_id       = ""
+# gcloud auth login
+# https://console.cloud.google.com/welcome?project=delta-fact-282601 (ID->delta-fact-282601)
+# habilitar compute Engiene: gcloud services enable compute.googleapis.com --project=delta-fact-282601
+# gcloud services disable compute.googleapis.com --project=delta-fact-282601
 
-  features {}
-}
-# crear usurio de app y darle permisos
-# az ad sp create-for-rbac --name terraform
-# az role assignment create --assignee 8a9c68c4-5478-4e02-b42e-a60c6e6e2573 --role "Resource Policy Contributor" --scope /subscriptions/8dcc0435-9f67-4310-9ca8-3cdc5fe6a09c
-# az role assignment create --assignee 8a9c68c4-5478-4e02-b42e-a60c6e6e2573 --role "Contributor" --scope /subscriptions/8dcc0435-9f67-4310-9ca8-3cdc5fe6a09c
-# az ad sp delete --id 8a9c68c4-5478-4e02-b42e-a60c6e6e2573
+# saber imagenes de ubuntu tiene (gcloud compute images list)
 
-resource "azurerm_resource_group" "example" {
-  name     = "myResourceGroup"
-  location = "East US"
+provider "google" {
+  # No es necesario especificar el archivo de credenciales cuando se usa la autenticación predeterminada de aplicación de Google Cloud
+  project = "delta-fact-282601" # Reemplaza "your-project-id" con el ID de tu proyecto en GCP
+  region  = "us-central1"       # Región de GCP que prefieras
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "myVnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "google_compute_network" "example_network" {
+  name = "my-network"
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "myPublicIP"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Static"
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "mySubnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "myNIC"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id # Asociar la dirección IP pública con la interfaz de red
+resource "google_compute_instance" "example_instance" {
+  name         = "my-instance"
+  machine_type = "n1-standard-1" # Tipo de máquina en GCP
+  zone         = "us-central1-a" # Zona de GCP que prefieras
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2204-lts" # Imagen de Ubuntu en GCP
+    }
+  }
+  network_interface {
+    network = google_compute_network.example_network.self_link
+    access_config {}
   }
 }
 
-resource "azurerm_linux_virtual_machine" "example" {
-  name                  = "myVM"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
-  size                  = "Standard_DS1_v2"
-  admin_username        = "adminuser"
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("/home/carlos/.ssh/id_rsa.pub")
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-}
-
-# Para saber su ip piblica.
-# az vm show -d --resource-group myResourceGroup --name myVM -o tsv --query publicIps
-# ssh adminuser@52.179.9.6
 
 # para que terraform no te pida el yes.
+# terraform init -upgrade
 # terraform apply -auto-approve
 # terraform destroy -auto-approve
+
+# par el ip (gcloud compute instances describe my-instance --project=delta-fact-282601 --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
