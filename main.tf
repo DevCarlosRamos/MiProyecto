5491ae2fa1dedfb1ca7adc165057ed6974629d29
Carlos Ramos <devcarlosramos@gmail.com>
2024-04-13 21:02:56 -0500

    feat: DEVI-5 create vm

--- main.tf
+++ main.tf
@@ -1,41 +1,86 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-
 provider "azurerm" {
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
+
   features {}
 }
+# crear usurio de app y darle permisos
+# az ad sp create-for-rbac --name terraform
+# az role assignment create --assignee 8a9c68c4-5478-4e02-b42e-a60c6e6e2573 --role "Resource Policy Contributor" --scope /subscriptions/8dcc0435-9f67-4310-9ca8-3cdc5fe6a09c
+# az role assignment create --assignee 8a9c68c4-5478-4e02-b42e-a60c6e6e2573 --role "Contributor" --scope /subscriptions/8dcc0435-9f67-4310-9ca8-3cdc5fe6a09c
+# az ad sp delete --id 8a9c68c4-5478-4e02-b42e-a60c6e6e2573
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
-  location            = "eastus"  # Ubicación del grupo de recursos
-  resource_group_name = "cloud-shell-storage-eastus"  # Nombre del grupo de recursos existente
-  kind                = "FunctionApp"
-  reserved            = true
+resource "azurerm_resource_group" "example" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
 
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
+resource "azurerm_virtual_network" "example" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id # Asociar la dirección IP pública con la interfaz de red
   }
 }
 
-resource "azurerm_app_service" "example" {
-  name                = "example-appservice-carlosramos-app"
-  location            = "eastus"  # Ubicación del grupo de recursos
-  resource_group_name = "cloud-shell-storage-eastus"  # Nombre del grupo de recursos existente
-  app_service_plan_id = azurerm_app_service_plan.example.id
+resource "azurerm_linux_virtual_machine" "example" {
+  name                  = "myVM"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  size                  = "Standard_DS1_v2"
+  admin_username        = "adminuser"
 
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-
-    # Configuración del contenedor Docker
-    app_command_line = ""
-    linux_fx_version = "DOCKER|devcarlosramos/auth-api:latest"  # Aquí se especifica la imagen Docker y la etiqueta
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("/home/carlos/.ssh/id_rsa.pub")
   }
 
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
 }
+
+# Para saber su ip piblica.
+# az vm show -d --resource-group myResourceGroup --name myVM -o tsv --query publicIps
+# ssh adminuser@52.179.9.6
+
+# para que terraform no te pida el yes.
+# terraform apply -auto-approve
+# terraform destroy -auto-approve
